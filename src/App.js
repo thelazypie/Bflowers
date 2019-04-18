364a1a63d954e8800abea21c11e67ba233240d78
Ryuunosuke <sven49376@gmail.com>
2019-04-18 11:49:17 +0300

    add about

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './Components/Header/Header'
 import Footer from './Components/Footer/Footer'
 import Main from './Components/Main/Main'
+import About from './Components/About/About'
 import Delivery from './Components/Delivery/Delivery'
 
 import { BrowserRouter, Route } from 'react-router-dom';
@@ -18,6 +19,7 @@ class App extends Component {
               {/* <Main/> */}
               <Route exact path='/' component={Main}/>
               <Route exact path='/delivery' component={Delivery}/>
+              <Route exact path='/about' component={About}/>
               <Route exact component={Main}/>
               {/* <Delivery/> */}
             <Footer/>
---
+++ src/Components/About/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+          <div className="About">
+            <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Ae00f2977a67cf1cf6bcbb92dc8f692a5cbcc49d986b22ed54396c0a4d77142f5&amp;source=constructor" width="100%" height="720" frameborder="0"></iframe>
+          </div>
+    );
+  }
+}
+
+export default App;
