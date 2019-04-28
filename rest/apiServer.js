3f76c693f29408272fed24ac221043135ef8831b
Ryuunosuke <sven49376@gmail.com>
2019-04-29 02:08:39 +0300

    added question form backend

--- rest/apiServer.js
+++ rest/apiServer.js
@@ -19,6 +19,15 @@ REST.post('/setNotify', (req,res)=>{
     // const data = JSON.parse(fs.readFileSync('./apiServerData/products.json'));
     // res.send({data:data});
 });
+REST.get('/setQuestion',(req,res)=>{
+    // console.log(req.query);
+    const data = JSON.parse(fs.readFileSync('./apiServerData/questions.json','utf8'));
+    if(!data.questions) {data.questions = []};
+    data.questions.push(req.query);
+    console.log(data.questions);
+    fs.writeFileSync('./apiServerData/questions.json',JSON.stringify(data));
+    res.send('successfully added');
+})
 
 REST.use(express.static('./apiServerData/staticData'));
 REST.use(bodyParser.urlencoded({ extended: true }));
